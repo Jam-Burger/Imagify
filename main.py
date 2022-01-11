baf028abf5bf664c64e927384fa7aab8ef5ace56
Jam-Burger <jay40793@gmail.com>
2022-01-11 20:43:21 +0530

    Created frame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 
 root= Tk()
-l1= Label(root, text="Hello on Github")
+img_window= LabelFrame(root, border= 1, text= "Image", padx=10, pady= 10)
+img_window.pack()
+l1= Label(img_window, text="This is in a frame")
 l1.pack()
 root.mainloop()
