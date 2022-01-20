ae4e30b90b02bd7f56f28e8ac3c0ae284cf42e3f
Jam_Burger <jay40793@gmail.com>
2022-01-20 18:05:57 +0530

    Added frame

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from tkinter import *
 
 root= Tk()
-root.geometry("300x300")
-img_window= LabelFrame(root, border= 1, text= "Image", padx=10, pady= 10)
-img_window.pack()
-l1= Label(img_window, text="This is in a frame")
-l1.pack()
+width= 800
+height= 600
+root.geometry(f"{width}x{height}")
+
+w = 500
+img_window= LabelFrame(root, border= 1, text= "Image", padx=10, pady= 10, width=w, height=w)
+img_window.grid(row=0, column=0, padx= 10, pady=10, rowspan=3)
+
+frame1= LabelFrame(root, border=1, padx= 10, pady= 10, width= width-w-20 , height= height/3)
+frame1.grid(row=0, column=1, padx= 5, pady=10)
+
+logo= Label(frame1, text="This is\na logo", width=(int)frame1.winfo_width)
+logo.grid(row= 0, column=0)
 root.mainloop()
