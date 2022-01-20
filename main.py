cc82498bd4f1b78c0a7f6bdecd7511e381451617
J-B-Github <Servpistag@gmail.com>
2022-01-20 17:38:34 +0530

    Added Something

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 
 root= Tk()
+root.geometry("300x300")
 img_window= LabelFrame(root, border= 1, text= "Image", padx=10, pady= 10)
 img_window.pack()
 l1= Label(img_window, text="This is in a frame")
