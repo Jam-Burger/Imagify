ad2faa76ff1377f07ecf2828a943ffec05646e69
Jam_Burger <jay40793@gmail.com>
2022-01-20 20:52:07 +0530

    Created logo and added image

--- editor.py
+++ editor.py
@@ -1,30 +1,32 @@
 from tkinter import *
-from PIL import *
+from PIL import Image, ImageTk
 
 import ctypes
-ctypes.windll.shcore.SetProcessDpiAwareness(2)
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
 root= Tk()
 width= 800
 height= 600
-root.geometry(f"{width}x{height}")
+# root.geometry(f"{width}x{height}")
 root.iconbitmap("images/logo.ico")
 root.title("Imagify - Jam-burger")
 # root.resizable(False, False)
 
 img_w = width*.6
 img_window= LabelFrame(root, border= 4, padx=10, pady= 10, width=img_w - 20, height=img_w - 20)
-img_window.grid(row=0, column=0, padx= 10, pady=10, rowspan=3)
+img_window.grid(row=0, column=0, padx= 10, pady=10, ipadx=5, ipady=5, rowspan=3)
+img_window.grid_propagate(0)
 
-frame1= LabelFrame(root, border=3, padx= 10, pady= 10, width= width - img_w - 20, height= img_w / 3 - 20)
-frame1.grid(row=0, column=1, padx= 10, pady=10)
+frame1= LabelFrame(root, border=3, padx= 10, pady= 10, width= width - img_w - 20, height= 120)
+frame1.grid(row=0, column=1, padx= 10, pady=10, ipadx=5, ipady=5)
 frame1.grid_propagate(0)
 
-logo= Label(frame1, text= "This is\na logo", image=None)
-logo.grid(row= 0, column=0, padx= 5, pady=5)
+img1= Image.open("images/Imagify-logo.jpeg")
+img1= ImageTk.PhotoImage(img1.resize((100, 100)))
+logo= Label(frame1, image= img1)
+logo.grid(row= 0, column=0, rowspan=2)
 
-
-name= Label(frame1, text="Jam-Burger")
-name.grid(row= 0, column=1, padx= 5, pady=5)
+Label(frame1, text="Imagify", font=('Comic Sans MS bold', 20)).grid(row= 0, column=1, padx= 5, pady=5, sticky=W)
+Label(frame1, text="By Jam-Burger", font=('Comic Sans MS', 10)).grid(row= 1, column=1, padx= 5, pady=5)
 
 root.mainloop()
