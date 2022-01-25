822c39b502dae21c61ed4ca20e6a06c185fe6b29
Jam_Burger <jay40793@gmail.com>
2022-01-25 12:03:31 +0530

    finally added cropability

--- editor.py
+++ editor.py
@@ -26,15 +26,17 @@ file_menu = Menu(menu_bar)
 menu_bar.add_cascade(label="File", menu=file_menu)
 file_menu.add_command(label="Open")
 
-iw_h = height - 140
-iw_w = iw_h*1.55
-c1_width = width - iw_w - 60
+i_f_w = height - 140
+i_f_h = int(i_f_w*1.55)
+c1_width = width - i_f_h - 60
 
 # creating basic layout
-
-img_window = Canvas(root, bg="#001", width=iw_w, height=iw_h)
-img_window.grid(row=0, column=0, rowspan=3)
-fns.set_img_window(img_window, iw_w, iw_h)
+img_window_frame= Frame(root, bg="#001", width=i_f_h, height=i_f_w)
+img_window_frame.grid(row=0, column=0, rowspan=3)
+img_window_frame.pack_propagate(0)
+img_window = Canvas(img_window_frame, bg='black', highlightthickness=0)
+img_window.pack(expand=1)
+fns.set_img_window(img_window, i_f_h, i_f_w)
 fns.change_img(Image.open("images/default.jpg"))
 
 imagify_frame = Frame(root, border=3, borderwidth=5, padx=10, pady=10,
@@ -56,7 +58,7 @@ dynamic_frame = Frame(root, border=3, borderwidth=5, bg=color_1,
 dynamic_frame.grid(row=2, column=1, padx=10, pady=(5, 0), sticky=N)
 
 crop_rotate_frame = Frame(dynamic_frame, bg=color_1,
-                          width=c1_width, height=height - 350 - main_frame_height)
+                          width=c1_width, height=height - 350 - main_frame_height, pady=20)
 crop_rotate_frame.pack()
 
 adjust_frame = Frame(dynamic_frame, bg='black',
@@ -92,40 +94,40 @@ save_image_btn = Button(main_frame, text="Save Image", command=fns.save_img)
 save_image_btn.grid(row=0, column=2, columnspan=2)
 btn_type1.append(save_image_btn)
 
-Label(main_frame, text="-"*36, font=('Consolas')).grid(row=1, column=0, columnspan=4)
+Label(main_frame, text="-"*50, font=('Consolas')).grid(row=1, column=0, columnspan=4)
 
-crop_rotate_btn = Button(main_frame, image=my_images.crop_rotate_img,
-                         command=lambda: fns.change_frame(dynamic_frame, crop_rotate_frame))
-crop_rotate_btn.grid(row=2, column=0, pady=(20, 0))
-Label(main_frame, text="Crop&\nRotate").grid(row=5, column=0)
-btn_type2.append(crop_rotate_btn)
+# crop_rotate_btn = Button(main_frame, image=my_images.crop_rotate_img,
+#                          command=lambda: fns.change_frame(dynamic_frame, crop_rotate_frame))
+# crop_rotate_btn.grid(row=2, column=0, pady=(20, 0))
+# Label(main_frame, text="Crop&\nRotate").grid(row=5, column=0)
+# btn_type2.append(crop_rotate_btn)
 
 undo_btn = Button(main_frame, image=my_images.undo_img, command=fns.undo)
 undo_btn.grid(row=2, column=1, pady=(20, 0))
-Label(main_frame, text="Undo").grid(row=5, column=1)
+Label(main_frame, text="Undo").grid(row=3, column=1)
 btn_type2.append(undo_btn)
 
 redo_btn = Button(main_frame, image=my_images.redo_img, command=fns.redo)
 redo_btn.grid(row=2, column=2, pady=(20, 0))
-Label(main_frame, text="Redo").grid(row=5, column=2)
+Label(main_frame, text="Redo").grid(row=3, column=2)
 btn_type2.append(redo_btn)
 
-adjust_btn = Button(main_frame, image=my_images.adjust_img,
-                    command=lambda: fns.change_frame(dynamic_frame, adjust_frame))
-adjust_btn.grid(row=2, column=3, pady=(20, 0))
-Label(main_frame, text="Adjust").grid(row=5, column=3)
-btn_type2.append(adjust_btn)
+# adjust_btn = Button(main_frame, image=my_images.adjust_img,
+#                     command=lambda: fns.change_frame(dynamic_frame, adjust_frame))
+# adjust_btn.grid(row=2, column=3, pady=(20, 0))
+# Label(main_frame, text="Adjust").grid(row=5, column=3)
+# btn_type2.append(adjust_btn)
 
-# resize_btn = Button(crop_rotate_frame, image=my_images.resize_img,
-#                     command=lambda: fns.resize_img(root))
-# resize_btn.grid(row=0, column=0, pady=(20, 0))
-# Label(crop_rotate_frame, text="Resize").grid(row=1, column=0)
-# btn_type2.append(resize_btn)
+resize_btn = Button(main_frame, image=my_images.resize_img,
+                    command=lambda: fns.resize_img(root))
+resize_btn.grid(row=2, column=0, pady=(20, 0))
+Label(main_frame, text="Resize").grid(row=3, column=0)
+btn_type2.append(resize_btn)
 
-# invert_btn = Button(main_frame, image=my_images.invert_img, command=fns.invert_img)
-# invert_btn.grid(row=2, column=3, pady=(20, 0))
-# Label(main_frame, text="Invert\nColor").grid(row=5, column=3)
-# btn_type2.append(invert_btn)
+invert_btn = Button(main_frame, image=my_images.invert_img, command=fns.invert_img)
+invert_btn.grid(row=2, column=3, pady=(20, 0))
+Label(main_frame, text="Invert\nColor").grid(row=3, column=3)
+btn_type2.append(invert_btn)
 
 for w in btn_type2:
     w.configure(width=42, height=42, anchor='nw')
@@ -139,7 +141,6 @@ for w in btn_type1:
 
 for i in range(4):
     crop_rotate_frame.columnconfigure(i, weight=1)
-
 
 rotate_a_btn = Button(crop_rotate_frame, image=my_images.rotate_a_img, command=fns.rotate_a_img)
 rotate_a_btn.grid(row=2, column=0)
@@ -156,6 +157,11 @@ flip_v_btn = Button(crop_rotate_frame, image=my_images.flip_v_img, command=fns.f
 flip_v_btn.grid(row=2, column=3)
 
 Label(crop_rotate_frame, text="Flip").grid(row=3, column=2, columnspan=2)
+
+crop_btn = Button(crop_rotate_frame, image=my_images.crop_rotate_img,
+                         command= fns.crop_img)
+crop_btn.grid(row=4, column=0, pady=(20, 0))
+Label(crop_rotate_frame, text="Crop").grid(row=5, column=0)
 
 for w in crop_rotate_frame.winfo_children():
     w.configure(font=('Consolas', 11), border=0,
--- functions.py
+++ functions.py
@@ -1,36 +1,43 @@
+from cmath import infj
+from re import I
 from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import filedialog, messagebox
 
 img_window = None
 current_img = None
-iw_w = iw_h = 0
+i_f_w = i_f_h = 0
 
 history_data = []
 current = -1
 
+
 def set_img_window(i_w, w_w, w_h):
-    global img_window, iw_w, iw_h
+    global img_window, i_f_w, i_f_h
     img_window = i_w
-    iw_w = w_w
-    iw_h = w_h
+    i_f_w = w_w
+    i_f_h = w_h
 
 
 def change_img(new_img, add_in_history=True):
-    # if not img_window: return
+    # if a very large image is given then resize
     i_w, i_h = new_img.size
     if i_w > 1400:
         new_img = new_img.resize((1400, int(1400*i_h/i_w)))
-    w_s = i_w/iw_w
-    h_s = i_h/iw_h
-    if w_s < h_s:
-        n_w = i_w/h_s
-        n_h = iw_h
-    else:
-        n_w = iw_w
-        n_h = i_h/w_s
+    elif i_h > 1400:
+        new_img = new_img.resize((int(1400*i_w/i_h), 1400))
 
-    global current_img, photo, current
+    i_w, i_h = new_img.size 
+    global current_img, photo, current, in_w, in_h
+    w_s = i_w/i_f_w
+    h_s = i_h/i_f_h
+    if w_s < h_s:
+        in_w = int(i_w/h_s)
+        in_h = i_f_h
+    else:
+        in_w = i_f_w
+        in_h = int(i_h/w_s)
+
     current_img = new_img
     if add_in_history:
         for e in history_data[current+1:]:
@@ -38,13 +45,76 @@ def change_img(new_img, add_in_history=True):
         history_data.append(current_img)
         current += 1
 
-    photo = ImageTk.PhotoImage(
-        current_img.resize((int(n_w) - 20, int(n_h) - 20)))
-    img_window.create_image(iw_w/2, iw_h/2, image= photo)
-
+    photo = ImageTk.PhotoImage(current_img.resize((in_w, in_h)))
+    img_window.create_image(in_w/2, in_h/2, image=photo)
+    img_window.configure(width= in_w, height= in_h)
 
 def change_frame(parent, frame):
     pass
+
+p1= None
+p2= None      
+
+rect= c1= c2 = None
+
+def finished(event):
+    img_window.unbind('<ButtonRelease-1>')
+    img_window.unbind('<B1-Motion>')
+    img_window.unbind('<Button 1>')
+    if rect:
+        img_window.delete(rect, c1, c2)
+    i_w, i_h= current_img.size
+
+    np1= (int(p1[0]*in_w/i_w), int(p1[1]*in_h/i_h))
+    np2= (int(p2[0]*in_w/i_w), int(p2[1]*in_h/i_h))
+
+    i_data = current_img.load()
+    width= np2[0] - np1[0]
+    height= np2[1] - np1[1]
+    dx= 0 if width==0 else int(abs(width)/width)
+    dy= 0 if height==0 else int(abs(height)/height)
+
+    cropped_img = Image.new(current_img.mode, (abs(width), abs(height)))
+
+    x = 0
+    for i in range(np1[0], np2[0], dx):
+        y = 0
+        for j in range(np1[1], np2[1], dy):
+            cropped_img.putpixel((x, y), i_data[i, j])
+            y += 1
+        x += 1
+        
+    change_img(cropped_img)
+    
+def move_p2(event):
+    global p2, rect, c1, c2
+    iw_w = img_window.winfo_width()
+    iw_h = img_window.winfo_height()
+    p2 = (event.x, event.y)
+    if p2[0] < 0: p2[0] = 0
+    elif p2[0] > iw_w - 1: p2[0] = iw_w - 1
+    if p2[1] < 0: p2[1] = 0
+    elif p2[1] > iw_h - 1: p2[1] = iw_h - 1
+
+    x1, y1= p1
+    x2, y2= p2
+    if rect:
+        img_window.delete(rect, c1, c2)
+    rect = img_window.create_rectangle(x1, y1, x2, y2, outline= 'white', width= 3)
+    c1 = img_window.create_oval(x1 - 5, y1 - 5, x1 + 5, y1 + 5, fill='black')
+    c2 = img_window.create_oval(x2 - 5, y2 - 5, x2 + 5, y2 + 5, fill='black')
+    img_window.bind('<ButtonRelease-1>', finished)
+
+def start_making(event):
+    global p1, p2
+    p1 = p2 = (event.x, event.y)
+    img_window.bind('<B1-Motion>', move_p2)
+
+def crop_img():
+    if current_img:
+        img_window.bind('<Button 1>', start_making)
+    else:
+        image_does_not_exist_msg()
 
 
 def rotate_a_img():
@@ -99,7 +169,7 @@ def flip_v_img():
         image_does_not_exist_msg()
 
 
-invert= lambda c: (255-c[0], 255-c[1], 255-c[2])
+def invert(c): return (255-c[0], 255-c[1], 255-c[2])
 
 
 def invert_img():
@@ -116,9 +186,12 @@ def invert_img():
 
 
 def resize_img(parent):
-    layer = Toplevel(parent)
-    layer.title("Select New Size")
-    Label(layer, text="I am in").pack()
+    not_done_msg()
+    # if current_img:
+    #     layer= Toplevel(img_window, width=200, height=200)
+    #     layer.resizable(0, 0)
+    # else:
+    #     image_does_not_exist_msg()
 
 
 def undo():
